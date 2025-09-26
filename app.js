faa291637b28f4d769044a5c1627e6a8f62de258
alextalijan <acatalijan@icloud.com>
2025-09-26 14:56:23 -0500

    feat: write essential code for app to listen for requests

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const indexRouter = require('./routes/indexRouter');
+
+// Set view engine to accept ejs files
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use('/', indexRouter);
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log('App listening for requests on port:', PORT);
+});
